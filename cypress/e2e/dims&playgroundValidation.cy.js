d661d917b39b4311eda1479ec586d01993bd245b
Olena Kateryniuk <lena.katerynyuk@gmail.com>
2024-06-27 16:59:12 +0300

    Test corrections

--- cypress/e2e/dims&playgroundValidation.cy.js
+++ cypress/e2e/dims&playgroundValidation.cy.js
@@ -7,29 +7,28 @@ describe('The Dims & payload column', () => {
     })
   })
 
-  it.only('should display correct information in the table', () => {
+  it('should display correct information in the table', () => {
     cy.intercept('GET', '/api/v1/trucks?page=1&page_size=10&archived=false').as('getTrucks');
     cy.wait('@getTrucks').its('response.statusCode').should('eq', 200);
     cy.get('@getTrucks').then(interception => {
-      const response = interception.response.body
+      const response = interception.response.body;
       const items = response.items;
 
-        items.forEach((item, index) => {
-          if (item.trailer) { 
-            const trailer = item.trailer;
-            cy.get('div[class=v-table__wrapper]') 
-              .find('tbody tr') 
-              .eq(index) 
-              .within(() => {
-                cy.get('[data-qa=truck-trailer-dims]').should('contain.text', trailer.length.toString());
-                cy.get('[data-qa=truck-trailer-dims]').should('contain.text', trailer.min_height.toString());
-                cy.get('[data-qa=truck-trailer-dims]').should('contain.text', trailer.min_width.toString())
-                cy.get('[data-qa=truck-trailer-dims]').siblings().should('contain.text', trailer.payload.toString() + ' lbs');
-              });
-          } else {
-            cy.log(`Property trailer in item with index ${index} equal null.`);
-          }
-        })
-    })
-  })
+      cy.get('div[class=v-table__wrapper] tbody tr').each(($row, index) => {
+        const item = items[index];
+        if (item.trailer) {
+          const trailer = item.trailer;
+          cy.wrap($row).within(() => {
+            // cy.get('[data-qa=truck-trailer-dims]').should('contain.text', trailer.length.toString() + '″ х ' + trailer.min_height.toString() + '″ x ' + trailer.min_width.toString() +'″ ');
+            cy.get('[data-qa=truck-trailer-dims]').should('contain.text', trailer.length.toString());
+            cy.get('[data-qa=truck-trailer-dims]').should('contain.text', trailer.min_height.toString());
+            cy.get('[data-qa=truck-trailer-dims]').should('contain.text', trailer.min_width.toString());
+            cy.get('[data-qa=truck-trailer-dims]').siblings().should('contain.text', trailer.payload.toString() + ' lbs');
+          });
+        } else {
+          cy.log(`Property trailer in item with index ${index} is null.`);
+        }
+      });
+    });
+  });
 })
--- cypress/e2e/filterTest.cy.js
+++ cypress/e2e/filterTest.cy.js
@@ -9,10 +9,12 @@ describe('The Trucks Filter', () => {
 
   it('should have "number" filter that works correctly with valid search word', () => {
     let searchWord = 'Truck1';
+
+    cy.intercept('GET', '/api/v1/trucks?number=Truck1&page=1&page_size=10&archived=false').as('getTrucks');
     cy.get('form').find('.v-row').children().contains('Main information');
     cy.get('[data-qa=number]').find('label').contains('Number').parent().type(searchWord);
     cy.get('[id=search--apply-btn]').click();
-    cy.wait(1000);
+    cy.wait('@getTrucks').its('response.statusCode').should('eq', 200);
     cy.get('a[data-qa=truck-number]').each(($el) => {
       cy.wrap($el).should('contain.text', searchWord);
     })
@@ -20,9 +22,11 @@ describe('The Trucks Filter', () => {
 
   it('should have "number" filter that works correctly with NOTvalid search word', () => {
     let searchWord = 'Truck78';
+
+    cy.intercept('GET', '/api/v1/trucks?number=Truck78&page=1&page_size=10&archived=false').as('getTrucks');
     cy.get('[data-qa=number]').find('label').contains('Number').parent().type(searchWord);
     cy.get('[id=search--apply-btn]').click();
-    cy.wait(1000);
+    cy.wait('@getTrucks').its('response.statusCode').should('eq', 200);
     cy.get('td[colspan=6]').contains('No data available')
   })
 })
